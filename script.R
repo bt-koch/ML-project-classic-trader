020a30668c26a60348d3767eb1479abdba50e108
bt-koch <bela.koch@hotmail.com>
2022-02-06 17:56:45 +0100

    update / improve training

--- script.R
+++ script.R
@@ -18,15 +18,14 @@ if(!require(rvest)) install.packages("rvest", repos = "http://cran.us.r-project.
 if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.org")
 if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
 if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
-# if(!require(performance)) install.packages("performance", repos = "http://cran.us.r-project.org")
-# see (for performance i.e. check_model)
+if(!require(randomForest)) install.packages("randomForest", repos = "http://cran.us.r-project.org")
 
 library(tidyverse)
 library(rvest)
 library(dplyr)
 library(data.table)
 library(caret)
-# library(performance)
+library(randomForest)
 
 # set seed (please uncomment depending on your R Version)
 # if using R 3.6 or later:
@@ -63,6 +62,13 @@ get_vehicle_information <- function(link){
 # objects
 data <- data.table()
 
+results <- data.table(
+  model = character(),
+  RMSE = numeric(),
+  MAE = numeric(),
+  MAPE = numeric()
+)
+
 
 
 # =============================================================================.
@@ -74,6 +80,8 @@ data <- data.table()
 # -----------------------------------------------------------------------------.
 
 if(scrape_data){
+  
+  cat("\nScrape data from classic-trader.com...")
   
   # scrape data
   base_link <- "https://www.classic-trader.com/uk/cars/search?fulltext=&pagination%5Bpage%5D="
@@ -111,15 +119,21 @@ if(scrape_data){
     
   }
   
+  cat("\nScraping successfully completed!")
+  
   # clean up
   rm(temp_dt, vehicles, vehicle_links, base_link, link, page, pages)
   
 } else {
   
+  cat("\nDownload dataset via github...")
+  
   # download "pre-scraped" data from github
   url <- "https://raw.githubusercontent.com/bt-koch/ML-project-classic-trader/main/scraped-data.csv"
   data <- read.csv(url, check.names = F)
   rm(url)
+  
+  cat(" download successful!")
 }
 
 rm(scrape_data)
@@ -133,6 +147,8 @@ rm(scrape_data)
 # -----------------------------------------------------------------------------.
 # 1.2 Data Wrangling ----
 # -----------------------------------------------------------------------------.
+cat("\nPrepare data...")
+
 saved <- copy(data) # delete when script is finished!
 
 # filter relevant data and perform simple manipulations
@@ -170,8 +186,12 @@ data <- data %>%
     ccm = as.integer(str_extract(ccm, pattern = "[\\.0-9e-]+")),
     # assume condition = "Original" when there is no further information
     condition = ifelse(is.na(condition), "Original", condition),
+    # create decade
+    decade = (year-1)-(year-1)%%10,
     # year as factor
     year = as.factor(year),
+    # decade as factor
+    decade = as.factor(decade),
     # number of cylinders as factor
     cylinders = as.factor(cylinders),
     # manufacturer as factor
@@ -282,6 +302,8 @@ manufac_top <- sort(manufac_top)
 par(mar = c(5.1, 7, 4.1, 2.1))
 barplot(manufac_top, horiz = T, las=1)
 
+manufac_top_53 <- manufac[1:53,]
+
 rm(manufac, manufac_top)
 
 # -----------------------------------------------------------------------------.
@@ -319,7 +341,7 @@ hist((development_set$power^lambda_power - 1)/lambda_power, probability = T, bre
 lines(density((development_set$power^lambda_power - 1)/lambda_power))
 
 # clean up
-rm(bxcx, lambda)
+rm(dens, bxcx)
 
 # -----------------------------------------------------------------------------.
 # 2.3 ccm ----
@@ -344,10 +366,12 @@ lines(density(1/development_set$ccm))
 
 # box-cox transformation
 bxcx <- BoxCoxTrans(development_set$ccm)
-lambda <- bxcx$lambda
-hist((development_set$ccm^lambda - 1)/lambda, probability = T, breaks = 25)
-lines(density((development_set$ccm^lambda - 1)/lambda))
+lambda_ccm <- bxcx$lambda
+hist((development_set$ccm^lambda_ccm - 1)/lambda_ccm, probability = T, breaks = 25)
+lines(density((development_set$ccm^lambda_ccm - 1)/lambda_ccm))
 
+# clean up
+rm(dens, bxcx)
 
 # -----------------------------------------------------------------------------.
 # 2.x manipulate features  ----
@@ -357,13 +381,26 @@ development_set <- development_set %>%
   mutate(
     price = log(price),
     power = (power^lambda_power - 1)/lambda_power
+  ) %>%
+  filter(manufacturer %in% manufac_top_53$Var1) %>%
+  select(-c(model, year)) %>%
+  mutate(
+    manufacturer = as.factor(as.character(manufacturer))
   )
 
 validation_set <- validation_set %>%
   mutate(
     price = log(price),
     power = (power^lambda_power - 1)/lambda_power
+  ) %>%
+  filter(manufacturer %in% manufac_top_53$Var1) %>%
+  select(-c(model, year)) %>%
+  mutate(
+    manufacturer = as.factor(as.character(manufacturer))
   )
+
+# clean up
+rm(lambda_power, lambda_ccm)
 
 # -----------------------------------------------------------------------------.
 # 2.x create train and test set ----
@@ -375,6 +412,8 @@ train_set <- development_set[-test_index,]
 
 rm(test_index)
 
+cat(" done!")
+
 # =============================================================================.
 # 3. Develop the model ----
 # =============================================================================.
@@ -382,9 +421,11 @@ rm(test_index)
 # -----------------------------------------------------------------------------.
 # 3.1 Linear Regression ----
 # -----------------------------------------------------------------------------.
+cat("\nTrain linear model...")
 
-train_set_test <- copy(train_set)[1:500,]
+# TO DELETE: check https://rpubs.com/j_fachrel/Linear-Regression-In-Used-Car-Price-Prediction <------
 
+# 3.1.1 train ---- 
 start_time <- Sys.time()
 cat("training lm started:", format(start_time, "%h.%m %H:%M"))
 
@@ -393,42 +434,122 @@ train_lm <- lm(price ~ ., data = train_set)
 end_time <- Sys.time()
 end_time - start_time
 
+# 3.1.2 analyze ----
 summary(train_lm)
-
 # check the default plots -> plot(train_lm, 1) ...
 
-# -----------------------------------------------------------------------------.
-# 3.2 Random Forrest ----
-# -----------------------------------------------------------------------------.
+# 3.1.3 predict ----
+# problem: when new factor in test set, prediction is not possible
+# -> remove observation with new factors
+test_set_lm <- copy(test_set)
 
+index <- which(!(test_set_lm$manufacturer %in% train_set$manufacturer))
+test_set_lm$manufacturer[index] <- NA
+
+# index <- which(!(test_set_lm$model %in% train_set$model))
+# test_set_lm$model[index] <- NA
+
+pred_lm <- predict(train_lm, test_set_lm)
+
+# 3.1.4 evaluate ----
+rmse <- RMSE(pred = pred_lm, obs = test_set_lm$price, na.rm = T)
+mae  <- MAE(pred = pred_lm, obs = test_set_lm$price, na.rm = T)
+mape <- mean(abs((test_set_lm$price-pred_lm)/test_set_lm$price), na.rm = T) * 100
+
+temp <- data.table(
+  model = "linear regression",
+  RMSE = rmse,
+  MAE = mae,
+  MAPE = mape
+)
+
+results <- rbind(results, temp)
+
+# clean up
+rm(start_time, end_time, index, rmse, mae, mape, temp)
+
+
+# -----------------------------------------------------------------------------.
+# 3.2 Random Forest ----
+# -----------------------------------------------------------------------------.
+cat("\nTrain random forest...")
+
+# it takes too long!
+# https://stats.stackexchange.com/questions/37370/random-forest-computing-time-in-r
+
+# 3.2.1 train ----
 start_time <- Sys.time()
 cat("training rf started:", format(start_time, "%h.%m %H:%M"))
 
-train_rf <- train(price ~ ., data = train_set, method = "rf")
+# train_rf <- train(price ~ ., data = train_set, method = "rf")
+train_rf <- randomForest(x = train_set[names(train_set) != "price"],
+                         y = train_set$price)
 
 end_time <- Sys.time()
 end_time - start_time
 
-# y_hat <- predict(train_rf, test_set)
+# 3.2.2 analyze ----
 print(train_rf)
 varImp(train_rf)
 
+# 3.2.3 predict ----
+# remove new factors necessary?
+pred_rf <- predict(train_rf, test_set)
+
+# 3.2.4 evaluate ----
+rmse <- RMSE(pred = pred_rf, obs = test_set$price, na.rm = T)
+mae  <- MAE(pred = pred_rf, obs = test_set$price, na.rm = T)
+mape <- mean(abs((test_set$price-pred_rf)/test_set$price), na.rm = T) * 100
+
+temp <- temp <- data.table(
+  model = "random forest",
+  RMSE = rmse,
+  MAE = mae,
+  MAPE = mape
+)
+
+results <- rbind(results, temp)
+
+# clean up
+rm(start_time, end_time, rmse, mae, mape, temp)
 
 # -----------------------------------------------------------------------------.
 # 3.3 K-nearest Neighbors ----
 # -----------------------------------------------------------------------------.
+cat("\nTrain knn...")
 
+# 3.3.1 train ----
 start_time <- Sys.time()
 cat("training knn started:", format(start_time, "%h.%m %H:%M"))
 
 train_knn <- train(price ~ ., data = train_set, method = "knn")
 
-
 end_time <- Sys.time()
 end_time - start_time
 
+# 3.3.2 analyze ----
 print(train_knn)
 varImp(train_knn)
+
+# 3.3.3 predict ----
+pred_knn <- predict(train_knn, test_set)
+
+# 3.3.4 evaluate ----
+rmse <- RMSE(pred = pred_knn, obs = test_set$price, na.rm = T)
+mae  <- MAE(pred = pred_knn, obs = test_set$price, na.rm = T)
+mape <- mean(abs((test_set$price-pred_knn)/test_set$price), na.rm = T) * 100
+
+temp <- temp <- data.table(
+  model = "knn",
+  RMSE = rmse,
+  MAE = mae,
+  MAPE = mape
+)
+
+results <- rbind(results, temp)
+
+# clean up
+rm(start_time, end_time, rmse, mae, mape, temp)
 
 # =============================================================================.
 # 4. Apply the model ----
